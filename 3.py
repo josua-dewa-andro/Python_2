8912dd72c988937657daf0d73f96799b9b241d9f
jojoE2412 <joshuaemma2412@gmail.com>
2025-08-30 00:14:56 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+angka = int(input('masukkan nilai: '))
+print("Tabel Perkalian " + str(angka) +':')
+
+for i in range (1, 4):
+    hasil = (angka*i)
+    print(str(angka)+' x '+ str(i) +' = '+ str(hasil))
+
+
